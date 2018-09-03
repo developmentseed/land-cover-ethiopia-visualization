92cd1489c46f27fe256924723ec997ad8a9f4c29
Rub21 <rub2106@gmail.com>
2018-09-03 18:51:00 -0500

    disply props

--- src/components/featureDetails.js
+++ src/components/featureDetails.js
@@ -3,18 +3,51 @@ import _ from 'underscore';
 import './styles.css';
 
 class FeatureDetails extends Component {
+    // diplayProperties = (feature) => {
+    //     const props = (keys, properties) => (
+    //         keys.map(key => (
+    //             <div className='rowPropertie'>
+    //             <div className='columnKeys'>{key}:</div><div className='columnValues'>{properties[key]}</div>
+    //             </div>
+    //         ))
+    //     )
+    //     return (
+    //         <div>
+    //             <h3>Properties</h3>
+    //         {props(_.keys(feature.properties), feature.properties)}
+    //         </div>
+    //     );
+    // }
+
+
+
     diplayProperties = (feature) => {
-        const props = (keys, properties) => (
-            keys.map(key => (
-                <div className='rowPropertie'>
-                <div className='columnKeys'>{key}:</div><div className='columnValues'>{properties[key]}</div>
+
+        const displayProps = (properties) => {
+            console.log(properties)
+            // _.keys(feature.properties),
+
+            return (
+                <div>
+                    <strong>
+                        {properties.WOREDANAME}
+                    </strong>
+                    
+                    <div>
+                        <span>Total area Inversores: </span>{properties.area_ha}
+                    </div>
+                    <div>
+                        <span>Total area OSM : </span>{properties.area_total_farms}
+                    </div>
+
                 </div>
-            ))
-        )
+                );
+        }
+
         return (
             <div>
-                <h3>Properties</h3>
-            {props(_.keys(feature.properties), feature.properties)}
+                {/* <h3>Properties</h3> */}
+                {displayProps(feature.properties)}
             </div>
         );
     }
@@ -23,7 +56,7 @@ class FeatureDetails extends Component {
         const { feature } = this.props;
         return (
             <div className='featureDetails'>
-                {/* {feature ? this.diplayProperties(feature) : <h1>select</h1>} */}
+                {feature ? this.diplayProperties(feature) : <h3>:</h3>}
             </div>
         );
     }
