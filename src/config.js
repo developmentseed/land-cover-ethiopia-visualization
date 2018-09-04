f5bd1f18605e1f6de4d1a28d678bd46eed5d8147
karitotp <karito@developmentseed.org>
2018-09-04 13:52:11 -0500

    modifying featureDetails

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const mapConfig = {
     accessToken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q',
     style: 'mapbox://styles/devseed/cjfwup8c518vc2slcclpfw3sp',
 };
-export const displayHeaders = ["REGIONNAME", "WOREDANAME", "area_total_farms","area_ha"];
+export const displayHeaders = ["REGIONNAME", "WOREDANAME", "area_ha", "number_farms", "area_total_farms",];
