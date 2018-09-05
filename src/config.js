c93ee2b24d89fa152f5596233be17792fb82c017
karitotp <karito@developmentseed.org>
2018-09-05 11:02:54 -0500

    adding mapbox style

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const mapConfig = {
     accessToken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q',
-    style: 'mapbox://styles/devseed/cjfwup8c518vc2slcclpfw3sp',
+    style: 'mapbox://styles/devseed/cjlp9k7nr7ij02rlgerg2e8r2',
 };
 
 export const propertiesToDisplay = {
